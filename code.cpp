3804ab76b3a70f9b539b8dd6f4fdbf3d0ce3d47b
srinilayapasupureedi <136550196+srinilayapasupureedi@users.noreply.github.com>
2024-11-24 23:02:41 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,18 +1,32 @@
- int maxSubarraySum(vector<int> &arr) {
-        // code here...
-        int maxSum=INT_MIN;
-        int sum=0;
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int find(vector<int>&arr)
+    {
+        int maxVal=INT_MIN;
         for(int i=0;i<arr.size();i++)
         {
-            sum+=arr[i];
-            if(sum>maxSum)
+            if(arr[i]>maxVal)
             {
-                maxSum=sum;
-            }
-            if(sum<0)
-            {
-                sum=0;
+                maxVal=arr[i];
             }
         }
-        return maxSum;
+        return maxVal;
     }
+    int getSecondLargest(vector<int> &arr) {
+        // Code Here
+        int maxVal=find(arr);
+        int secLargest=-1;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]!=maxVal&&arr[i]>secLargest)
+            {
+                secLargest=arr[i];
+                
+            }
+            
+        }
+        return secLargest;
+    }
+};
